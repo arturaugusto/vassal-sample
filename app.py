6f83e85f141ffcb6dafbc8d42c37b743241d0967
Artur <arturaugusto@gmail.com>
2018-03-03 16:52:10 -0300

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ application = Flask(__name__)
 
 @application.route("/", methods=['POST','GET'])
 def hello():
-  return 'hello!!!x'
+  return 'hello!!!xx'
 
 if __name__ == "__main__":
   application.run(host='0.0.0.0')
